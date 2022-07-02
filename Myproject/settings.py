901605dc1c8520c089cef2cd44fe99f4f44a0f58
John Elro Karl Estoque <44110280+johnelro@users.noreply.github.com>
2022-07-02 21:57:59 +0800

    Update settings.py

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':  'postgres',
         'USER':  'postgres',
-        'PASSWORD': 'Elro2001',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
